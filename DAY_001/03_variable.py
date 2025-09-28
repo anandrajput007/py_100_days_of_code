1ec7ca5bb0008aa3900c62d0ae60eb082cdc6dee
Anand Rajput <iamanandjee@gmail.com>
2025-09-28 20:02:58 +0530

    Day 01 code challanges

---
+++ DAY_001/03_variable.py
@@ -0,0 +1,5 @@
+name = input("what is your name?")
+message = "Hello " + name + "!"
+message_length = len(message)
+print(message)
+print("Length of message is - " , message_length)
---
+++ DAY_001/04_brand_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand Name Generator.")
+color = input("What is your favourite color?\n")
+city = input("What is your favourite city?\n")
+print("Your brand name could be: " + color + " " + city)
